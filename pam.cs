d7166486b1ed89d33eeee500de508ffafdd5723f
YellowApps <96654406+YellowApps@users.noreply.github.com>
2022-08-28 12:04:26 +0300

    Add files via upload

--- pam.cs
+++ pam.cs
@@ -13,18 +13,24 @@ namespace pam{
       string dir = Environment.GetEnvironmentVariable("appdata") + "\\" + mainFile.Split('.')[0] + "\\";
 
       Console.WriteLine("PAM 1.0 started.");
-      Console.WriteLine("Directory: "+dir);
+      Console.WriteLine("Destination: "+dir);
 
       if(!Directory.Exists(dir)) Directory.CreateDirectory(dir);
       if(!File.Exists(dir+mainFile)){
         for(int i = 0; i < files.Length; i++){
           byte[] data = Convert.FromBase64String(contents[i]);
 
-          Console.WriteLine(files[i]);
+          Console.WriteLine("Unpacking: " + files[i]);
           File.WriteAllBytes(dir + files[i], data);
         }
       }
-      Process.Start("cmd", "/c \"cd /d "+dir+" & start "+mainFile+"\"");
+      Process proc = new Process();
+      proc.StartInfo.UseShellExecute = false;
+      proc.StartInfo.FileName = "cmd.exe";
+      proc.StartInfo.Arguments = "/c \"cd /d "+dir+" & start "+mainFile+"\"";
+      proc.StartInfo.CreateNoWindow = false;
+      proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+      proc.Start();
     }
   }
 }
